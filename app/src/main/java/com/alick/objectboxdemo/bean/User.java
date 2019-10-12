bacef4385aa5e7847c7d897b92ea64c39fca6d77
cuixw <cuixw@guahao.com>
2019-10-12 17:01:45 +0800

    增加注释

--- app/src/main/java/com/alick/objectboxdemo/bean/User.java
+++ app/src/main/java/com/alick/objectboxdemo/bean/User.java
@@ -2,6 +2,7 @@ package com.alick.objectboxdemo.bean;
 
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
+import io.objectbox.annotation.Index;
 
 /**
  * @author 崔兴旺
@@ -11,10 +12,11 @@ import io.objectbox.annotation.Id;
 @Entity
 public class User {
     @Id//必须设置@Id注解
-    private long _id;//必须是long类型
-    private String username;
+    private long    _id;        //必须是long类型
+    @Index//当频繁根据此字段查询时,推荐加上索引以提高查询效率
+    private String  username;
     private boolean sex;
-    private int age;
+    private int     age;
 
     public User(String username, boolean sex, int age) {
         this.username = username;
